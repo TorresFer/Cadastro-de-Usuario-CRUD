02eb8be928cff7b7d5bdc8497e4adaaecf51b7ff
Fernando Torres <fernandorox1@hotmail.com>
2022-05-30 22:13:28 -0300

    third commit

---
+++ src/core/ClienteRepositorio.ts
@@ -0,0 +1,12 @@
+import Cliente from "./Cliente"
+
+
+export default interface ClienteRepositorio{
+    salvar(cliente: Cliente): Promise<Cliente>
+    excluir(cliente: Cliente): Promise<void>
+    obterTodos(cliente: Cliente): Promise<Cliente[]>
+}
+
+//Interface criada direta no TS para se referir ao DB do Firebase com o intuito
+//de facilitar caso eu queira continuar ampliando o projeto; caso contrário
+//poderia simplesmente fazer em um componente.
